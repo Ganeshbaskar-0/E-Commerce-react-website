a24891e68f1cbbd18274d81ae0a61334bd0f2be7
Ganeshbaskar-0 <Baskarganesh2003@gmail.com>
2026-06-05 13:51:06 +0530

    Added Authentication System

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Home from "./pages/Home"
 import Auth from "./pages/Auth"
 import Checkout from "./pages/Checkout"
 import Navbar from "./components/Navbar"
+import AuthProvider from "./context/AuthContext"
 
 export default function App()
 {
     return (
         <div className="App">
+            <AuthProvider>
             <Navbar/>
             <Routes>
                 <Route  path="/" element={<Home/>}/>
                 <Route  path="/auth" element={<Auth/>}/>
-                <Route  path="/checkout" element={< Checkout/>}/>
+                <Route  path="/checkout" element={<Checkout/>}/>
             </Routes>
+            </AuthProvider>
 
         </div>
     )
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,61 @@
+import React, { createContext, useState } from 'react'
+
+export const AuthContext = createContext(null);
+const AuthProvider = ({children}) => {
+
+    const [user,setUser] = useState(
+        localStorage.getItem("currentUserEmail")
+        ?{email:localStorage.getItem("currenUserEmail")}:
+    null
+    );
+
+function signUp(email,password)
+{
+    const users = JSON.parse(localStorage.getItem("users" || "[]"))
+    if(users.find((u)=> u.email===email))
+    {
+        return {success:false,error:"Email Already Exists"}
+    }
+
+    const newUser = {email,password}
+    users.push(newUser)
+    localStorage.setItem("users",JSON.stringify(users))
+    localStorage.setItem("currentUserEmail",email)
+
+    
+    setUser({email});
+    return {success:true }
+
+}
+function login(email,password)
+{
+    const users = JSON.parse(localStorage.getItem("users"||"[]"));
+    const user = users.find(
+        (u)=> u.email===email && u.password===password
+    )
+    if(!user)
+    {
+        return {success:false,error:"Invalid username or password"}
+    }
+
+    localStorage.setItem("currentuserEmail",email)
+    setUser(email)
+    return {success:true}
+}
+
+function logout()
+{
+    localStorage.removeItem("currentUserEmail");
+    setUser(null)
+}
+
+
+
+  return (
+    <AuthContext.Provider value={ {signUp, user ,logout,login} }>{children}</AuthContext.Provider>
+
+    
+  )
+}
+
+export default AuthProvider
